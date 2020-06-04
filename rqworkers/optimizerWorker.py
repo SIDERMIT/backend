bdb1e03564c3b00cd95ac4fa05b99a0290ff1e4d
Felipinbombin <cephei.1313@gmail.com>
2020-06-04 14:20:07 -0400

    refactor

--- rqworkers/optimizerWorker.py
+++ rqworkers/optimizerWorker.py
@@ -8,10 +8,10 @@ class OptimizerWorker(Worker):
     def __init__(self, queues, name=None, default_result_ttl=None, connection=None,
                  exc_handler=None, exception_handlers=None, default_worker_ttl=None,
                  job_class=None, queue_class=None):
-        print("Initializing HookWorker...")
+        print("Initializing OptimizerWorker...")
         super(OptimizerWorker, self).__init__(queues, name=name, default_result_ttl=default_result_ttl,
                                               connection=connection, exc_handler=exc_handler,
                                               exception_handlers=exception_handlers,
                                               default_worker_ttl=default_worker_ttl, job_class=job_class,
                                               queue_class=queue_class)
-        print("HookWorker ready!")
+        print("OptimizerWorker ready!")
