c1a189dcd55dd34133b322e9498eea5f186b23b9
Felipinbombin <cephei.1313@gmail.com>
2020-08-13 17:47:20 -0400

    added type attribute to node data in network descriptor

--- api/utils.py
+++ api/utils.py
@@ -1,3 +1,6 @@
+from sidermit.city.graph import CBD, Periphery, Subcenter
+
+
 def get_network_descriptor(graph_obj):
     """
 
@@ -6,7 +9,15 @@ def get_network_descriptor(graph_obj):
     """
     nodes = []
     for node_obj in graph_obj.get_nodes():
-        node_descriptor = dict(name=node_obj.name, id=node_obj.id, x=node_obj.x, y=node_obj.y)
+        node_type = None
+        if isinstance(node_obj, CBD):
+            node_type = 'cbd'
+        elif isinstance(node_obj, Periphery):
+            node_type = 'periphery'
+        elif isinstance(node_obj, Subcenter):
+            node_type = 'subcenter'
+
+        node_descriptor = dict(name=node_obj.name, id=node_obj.id, x=node_obj.x, y=node_obj.y, type=node_type)
         nodes.append(node_descriptor)
 
     edges = []
