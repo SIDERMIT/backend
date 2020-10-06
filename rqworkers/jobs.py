c62ed5f3e4bda9662f3222de1abddbe69f5e6e8f
Felipinbombin <cephei.1313@gmail.com>
2020-10-06 18:13:20 -0300

    implement logic to record execution

--- rqworkers/jobs.py
+++ rqworkers/jobs.py
@@ -14,6 +14,7 @@ logger = logging.getLogger(__name__)
 
 @job(settings.OPTIMIZER_QUEUE_NAME)
 def optimize_transport_network(transport_network_public_id):
+    start_time = timezone.now()
     transport_network_obj = TransportNetwork.objects.select_related('scene__city', 'scene__passenger').get(
         public_id=transport_network_public_id)
     transport_network_obj.optimization_status = TransportNetwork.STATUS_PROCESSING
@@ -26,6 +27,13 @@ def optimize_transport_network(transport_network_public_id):
     passenger = transport_network_obj.scene.passenger.get_sidermit_passenger()
     network = transport_network_obj.get_sidermit_network(graph)
 
+    transport_mode_dict = dict()
+    for transport_mode_obj in transport_network_obj.scene.transportmode_set.all():
+        transport_mode_dict[transport_mode_obj.id] = transport_mode_obj.get_sidermit_transport_mode()
+
+    for route_obj in transport_network_obj.route_set.all():
+        network.add_route(route_obj.get_sidermit_route(transport_mode_dict[route_obj.transport_mode_id]))
+
     try:
         # build optimizer object
         opt_obj = Optimizer(graph, demand, passenger, network, f=None)
@@ -34,25 +42,38 @@ def optimize_transport_network(transport_network_public_id):
 
         ov_results = opt_obj.overall_results(res)
 
-        opt_result_obj, _ = OptimizationResult.objects.get_or_create(transport_network=transport_network_obj)
-        opt_result_obj.vrc = ov_results['VRC']
-        opt_result_obj.co = ov_results['operators_cost']
-        opt_result_obj.ci = ov_results['infraestructure_cost']
-        opt_result_obj.cu = ov_results['users_cost']
-        opt_result_obj.tv = ov_results['travel_time_on_board']
-        opt_result_obj.tw = ov_results['waiting_time']
-        opt_result_obj.ta = ov_results['access_time']
-        opt_result_obj.t = ov_results['transfers']
-        opt_result_obj.save()
+        opt_result_obj, created = OptimizationResult.objects.get_or_create(
+            transport_network=transport_network_obj,
+            defaults=dict(vrc=ov_results['VRC'], co=ov_results['operators_cost'], ci=ov_results['infrastructure_cost'],
+                          cu=ov_results['users_cost'], tv=ov_results['travel_time_on_board'],
+                          tw=ov_results['waiting time'], ta=ov_results['access_time'], t=ov_results['transfers']))
+        if not created:
+            opt_result_obj.vrc = ov_results['VRC']
+            opt_result_obj.co = ov_results['operators_cost']
+            opt_result_obj.ci = ov_results['infrastructure_cost']
+            opt_result_obj.cu = ov_results['users_cost']
+            opt_result_obj.tv = ov_results['travel_time_on_board']
+            opt_result_obj.tw = ov_results['waiting time']
+            opt_result_obj.ta = ov_results['access_time']
+            opt_result_obj.t = ov_results['transfers']
+            opt_result_obj.save()
 
         for mode in ov_results['vehicles_mode']:
-            transport_mode_obj = TransportMode.objects.filter(scene=transport_network_obj.scene).get(name=mode.name)
-            opt_per_mode = OptimizationResultPerMode.objects.get_or_create(transport_network=transport_network_obj,
-                                                                           transport_mode=transport_mode_obj)
-            opt_per_mode.b = ov_results['vehicles_mode'][mode]
-            opt_per_mode.k = ov_results['vehicle_capacity_mode'][mode]
-            opt_per_mode.l = ov_results['lines_mode'][mode]
-            opt_per_mode.save()
+            b = ov_results['vehicles_mode'][mode]
+            k = ov_results['vehicle_capacity_mode'][mode]
+            l = ov_results['lines_mode'][mode]
+
+            transport_mode_obj = TransportMode.objects.get(scene=transport_network_obj.scene, name=mode.name)
+            opt_per_mode, created = OptimizationResultPerMode.objects.get_or_create(
+                transport_network=transport_network_obj,
+                transport_mode=transport_mode_obj,
+                defaults=dict(b=b, k=k, l=l))
+
+            if not created:
+                opt_per_mode.b = b
+                opt_per_mode.k = k
+                opt_per_mode.l = l
+                opt_per_mode.save()
 
         network_results = opt_obj.network_results(res)
 
@@ -60,7 +81,9 @@ def optimize_transport_network(transport_network_public_id):
             route_name = route[0]
             route_obj = Route.objects.get(transport_network=transport_network_obj, name=route_name)
             opt_result_per_route_obj, _ = OptimizationResultPerRoute.objects.get_or_create(
-                transport_network=transport_network_obj, route=route_obj)
+                transport_network=transport_network_obj, route=route_obj,
+                defaults=dict(frequency=route[1], k=route[2], b=route[3], tc=route[4], co=route[5],
+                              lambda_min=route[6]))
             opt_result_per_route_obj.frequency = route[1]
             opt_result_per_route_obj.k = route[2]
             opt_result_per_route_obj.b = route[3]
@@ -83,7 +106,13 @@ def optimize_transport_network(transport_network_public_id):
                                                                 direction=OptimizationResultPerRouteDetail.DIRECTION_R,
                                                                 origin_node=node_i, destination_node=node_j,
                                                                 lambda_value=charge_ij)
+
+            transport_network_obj.optimization_status = TransportNetwork.STATUS_FINISHED
+            transport_network_obj.optimization_duration = timezone.now() - start_time
+            transport_network_obj.optimization_error_message = None
+            transport_network_obj.save()
     except SIDERMITException as e:
         transport_network_obj.optimization_status = TransportNetwork.STATUS_ERROR
+        transport_network_obj.optimization_duration = timezone.now() - start_time
         transport_network_obj.optimization_error_message = str(e)
         transport_network_obj.save()
