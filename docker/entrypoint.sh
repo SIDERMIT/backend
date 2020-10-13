15a92257a7bf6abd2d8cc103bfb6bcae54c1d710
Felipinbombin <cephei.1313@gmail.com>
2020-10-13 19:29:33 -0300

    added function to wait for services

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,20 +1,5 @@
-wait_for_port()
-{
-  local name="$1" host="$2" port="$3"
-  local j=0
-  while ! nc -z "$host" "$port" >/dev/null 2>&1 < /dev/null; do
-    j=$((j+1))
-    if [ $j -ge $TRY_LOOP ]; then
-      echo >&2 "$(date) - $host:$port still not reachable, giving up"
-      exit 1
-    fi
-    echo "$(date) - waiting for $name... $j/$TRY_LOOP"
-    sleep 5
-  done
-}
-
-#wait_for_port "postgres" "db" "5432"
-#wait_for_port "redis" "cache" "6379"
+/app/docker/wait-for.sh db:5432 -- echo "database is available"
+/app/docker/wait-for.sh cache:6379 -- echo "cache is available"
 
 case "$1" in
   webserver)
