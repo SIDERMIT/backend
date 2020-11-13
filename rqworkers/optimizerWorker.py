2d893f2dcc584fa8afc91dfcb6832bd9d628d46f
Felipinbombin <cephei.1313@gmail.com>
2020-11-13 19:44:08 -0300

    use KillWorker class to kill running jobs

--- rqworkers/optimizerWorker.py
+++ rqworkers/optimizerWorker.py
@@ -1,9 +1,9 @@
-from rq import Worker
+from rqworkers.killClass import KillWorker
 
 
 # Same worker called with rqworker by default, created to be able to call it in the same way
 # as any other worker in the script for the service.
-class OptimizerWorker(Worker):
+class OptimizerWorker(KillWorker):
 
     def __init__(self, queues, name=None, default_result_ttl=None, connection=None,
                  exc_handler=None, exception_handlers=None, default_worker_ttl=None,
